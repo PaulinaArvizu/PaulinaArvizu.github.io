2411fd8e1572cdd2ba0fd03b0855129d4f361663
Héctor Gerardo Silva Ayala <39529919+hgSilv@users.noreply.github.com>
2019-11-24 06:20:26 -0600

    Genera las funciones necesarias para el Editar Usuario.js

---
+++ Administrador - Editar Usuarios.js
@@ -0,0 +1,179 @@
+'use strict'
+const baseURL = 'http://localhost:3000';
+document.body.onload = loadUsers;
+let detailedUsers = [];
+
+let mBtnBorrar = document.getElementById("userDel");
+let mBtnUpdate = document.getElementById("userUpdate");
+
+let hoy = new Date();
+
+//EVENT LISTENERS
+mBtnUpdate.addEventListener('click', DoUpdateUser);
+mBtnBorrar.addEventListener('click', DoDeleteUser);
+
+function loadUsers(event) {
+    makeHTTPRequest('/usuarios', 'GET', '', listUsers);
+    loadUser();
+}
+
+
+//FUNCTIONS
+
+function userListToHTML(users) {
+    let arrUsers = users.map(function (user) {
+        return userToHTML(user);
+    });
+    document.getElementById('ListaDeUsuarios').innerHTML = arrUsers.join("");
+}
+
+function userToHTML(user) {
+    let fechaNacimiento = new Date(user.fecha);
+    let edad = Math.floor((hoy - fechaNacimiento)/31557600000);
+    let ret = `<table class="table table-condensed usuario" width="50%">
+    <tbody>
+        <tr>
+            <th>
+                <img src="Imagenes/Seguidor1.jpg">
+                <p>${user.nombre}</p>
+            </th>
+            <td>
+                <p><b>Especifiaciones</b></p>
+                <ul type="none">
+                    <li>Correo: ${user.correo}</li>
+                    <li>Edad: ${edad}</li>
+                </ul>
+            </td>
+            <td>
+                <p><b>Descripción</b></p>
+                <p>Temporalmente vacio</p>
+            </td>
+            <td>
+                <p><button type="button" class="btn btn-secondary" id="btnMakeAdmin" onclick="updateUser('${user.id}')" data-toggle="modal" data-target="#update-User">Cambiar a administrador</button></p>
+                <p><button type="button" class="btn btn-danger" id="btnDeleteUser" onclick="deleteUser('${user.id}')" data-toggle="modal" data-target="#delete-User">Eliminar usuario</button></p>
+            </td>
+        </tr>
+    </tbody>
+</table>`;
+    return ret;
+}
+
+
+function deleteUser(id) {
+    makeHTTPRequest(`/usuarios/${id}`, 'GET', '', cbToDeleteUser);
+}
+
+function updateUser(id) {
+    makeHTTPRequest(`/usuarios/${id}`, 'GET', '', cbToUpdateUser);
+}
+
+function DoDeleteUser(event) {
+    console.log("entro");
+    console.log(document.getElementById("delete-id-user").innerText);
+    let userId = document.getElementById("delete-id-user").innerText;
+    makeHTTPRequest(`/usuarios/${userId}`, 'DELETE', '', cbDoDeleteUser);
+}
+
+function DoUpdateUser(event) {
+    console.log("entro");
+    console.log(document.getElementById("update-id-user").innerText);
+    let userId = document.getElementById("update-id-user").innerText;
+    let newUser = {
+        admin: "true"
+    };
+    makeHTTPRequest(`/usuarios/${userId}`, 'PATCH', newUser, cbDoUpdateUser);
+}
+
+//REQUEST
+function makeHTTPRequest(endpoint, method, data, callback) {
+    console.log(method);
+    let headers = {
+        'Content-Type': 'application/json'
+    };
+    if (method == 'DELETE')
+        headers = '';
+    // 1. crear XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+    // 2. configurar: PUT actualizar archivo
+    xhr.open(method, baseURL + endpoint);
+    for (let key in headers) {
+        xhr.setRequestHeader(key, headers[key]);
+        xhr.setRequestHeader
+    }
+    // 4. Enviar solicitud
+    console.log(data);
+    console.log(headers);
+    xhr.send(JSON.stringify(data));
+    // 5. Una vez recibida la respuesta del servidor
+    xhr.onload = () => {
+        callback(xhr);
+    }
+}
+
+
+//CALLBACKS
+
+
+function listUsers(xhr) {
+
+    switch (xhr.status) {
+        case 200:
+            let uList = JSON.parse(xhr.response);
+            uList.forEach(element => {
+                let userId = element.id;
+                makeHTTPRequest(`/usuarios/${userId}`, 'GET', '', userDetails);
+            });
+            break;
+        default:
+            alert("Error " + xhr.status);
+    }
+    return xhr.status;
+}
+
+
+function userDetails(xhr) {
+    switch (xhr.status) {
+        case 200:
+            let newUser = JSON.parse(xhr.response);
+            detailedUsers.push(newUser);
+            userListToHTML(detailedUsers);
+    }
+    return xhr.status;
+}
+
+
+function cbDoDeleteUser(xhr) {
+    switch(xhr.status)
+        {
+            case 200: console.log("Exitoso: " + xhr.status);
+                alert("Usuario Eliminado");
+                window.location.reload(true);
+                break;
+            default:
+                alert("Error "+ xhr.status);
+        }
+        return xhr.status;
+}
+
+function cbDoUpdateUser(xhr) {
+    switch(xhr.status)
+        {
+            case 200: console.log("Exitoso: " + xhr.status);
+                alert("Usuario actualizado");
+                window.location.reload(true);
+                break;
+            default:
+                alert("Error "+ xhr.status);
+        }
+        return xhr.status;
+}
+
+function cbToUpdateUser(xhr) {
+    console.log(xhr.response);
+    let user2up = JSON.parse(xhr.response);
+    document.getElementById("update-nombre-user").innerText = user2up.nombre;
+    document.getElementById("update-id-user").innerText = user2up.id;
+
+    return xhr.status;
+
+}
