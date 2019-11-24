9ea1f220ebfc67f7fe6f965e1a058d146edb510b
Alderbane <alderban@outlook.com>
2019-11-24 01:50:45 -0600

    Script usuario ver cuenta

---
+++ Usuario - ver cuenta.js
@@ -0,0 +1,156 @@
+let user;
+let userId = 0;
+let baseURL = "http://localhost:3000/";
+
+function makeHTTPRequest(endpoint, method, data, cbOk, cbErr) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, baseURL + endpoint);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+    xhr.onload = () => {
+        if (xhr.status != 200) {
+            cbErr(xhr.status, xhr.statusText);
+        } else {
+            cbOk(xhr.responseText);
+        }
+    }
+}
+
+
+makeHTTPRequest('usuarios/' + userId, 'GET', undefined,
+    kk => {
+        user = JSON.parse(kk);
+        // displayPost;
+        makeHTTPRequest('publicaciones?idU=' + userId, 'GET', undefined,
+            kk => {
+                console.log(kk)
+                let postMap = JSON.parse(kk).map(x => htmlPublicacion(x));
+                document.querySelector(".publicaciones").innerHTML = "<h3>Tus publicaciones</h3>" + postMap.join(" ");
+            },
+            nok => {
+
+            });
+
+        makeHTTPRequest('mascotas?idU=' + userId, 'GET', undefined,
+            kk => {
+                console.log(kk)
+                let petMap = JSON.parse(kk).map(x => htmlPet(x));
+                document.querySelector(".tus-mascotas table tbody").innerHTML = petMap.join(" ");
+            },
+            nok => {
+            })
+        makeHTTPRequest('pedidos?idU=' + userId, 'GET', undefined,
+            kk => {
+                console.log(kk)
+                let a = JSON.parse(kk);
+                let pOrderMap = a.filter(e => e.status == 2).map(e => htmlPendingOrders(e))
+                document.querySelector(".pedidos-pendientes").innerHTML = pOrderMap.join(" ");
+                let cOrderMap = a.filter(e => e.status == 3).map(e => htmlPendingOrders(e))
+                document.querySelector(".historial-pedidos").innerHTML = cOrderMap.join(" ");
+            },
+            nok => {
+            })
+    },
+    nok => {
+        alert('No existe el usuario');
+    });
+
+
+
+// let displayPost = makeHTTPRequest('publicaciones?idU=' + userId, 'GET', undefined,
+//     kk => {
+//         console.log(kk)
+//         let postMap = JSON.parse(kk).map(x => htmlPublicacion(x));
+//         document.querySelector(".publicaciones").innerHTML = "<h3>Tus publicaciones</h3>" + postMap.join(" ");
+//     },
+//     nok => {
+
+//     })
+
+
+function htmlPublicacion(post) {
+    return `<div class="card">
+    <div class="card-header">
+        ${user.nombre}
+    </div>
+    <img src="${post.img}"" class=" card-img-top" alt="foto publicacion ${post.id}"">
+    <div class=" card-body">
+        <p class="mascota">${post.nombreMascotas==undefined?"":post.nombreMascotas}</p>
+        <p class="card-text">${post.descripcion}</p>
+        <table width="100%">
+            <tr>
+                <td><a href="#" style="color: brown;""><i class=" fas fa-paw"></i> Me gusta</a></td>
+                <td align="right"><a class="delete" href="#" style="color: red;" onclick="deletePost(${post.id})"><i class="fas fa-times-circle"></i> Eliminar</a></td>
+            </tr>
+        </table>
+    </div>
+</div>`
+}
+
+function htmlPet(pet) {
+    return `<tr>
+    <td scope="row"><img src="${pet.img}" alt=""></td>
+    <td class="mascota">${pet.nombre}</td>
+    </tr>`
+}
+
+function deletePost(id) {
+    makeHTTPRequest('publicaciones/' + id, 'DELETE', undefined,
+        kk => {
+            console.log(kk)
+            makeHTTPRequest('publicaciones?idU=' + userId, 'GET', undefined,
+                kk => {
+                    console.log(kk)
+                    let postMap = JSON.parse(kk).map(x => htmlPublicacion(x));
+                    document.querySelector(".publicaciones").innerHTML = "<h3>Tus publicaciones</h3>" + postMap.join(" ");
+                },
+                nok => {
+                    console.log(nok)
+                })
+        },
+        nok => {
+            console.log(nok)
+        })
+}
+
+function htmlPendingOrders(order) {
+    return `<h3>Pedidos en curso</h3>
+    <div class="card border-warning mb-3">
+        <div class="card-header">Pedido: #${order.id}</div>
+        <div class="card-body">
+        <table class="table-striped table-condensed" width="100%">
+    <thead>
+        <tr>
+            <th>Cantidad</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+        </tr>
+    </thead>
+    <tbody>` + order.productos.map(x => `<tr><td scope="row">${x.cantidad}</td><td>${x.nombre}</td><td>${x.precio}</td></tr>`).join(' ') + `</tbody>
+    </table>`;
+}
+
+function htmlCompleteOrders(order) {
+    return `<h3>Pedidos en curso</h3>
+    <div class="card border-success mb-3">
+        <div class="card-header">Pedido: #${order.id}</div>
+        <div class="card-body">
+        <table class="table-striped table-condensed" width="100%">
+    <thead>
+        <tr>
+            <th>Cantidad</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+        </tr>
+    </thead>
+    <tbody>` + order.productos.map(x => `<tr><td scope="row">${x.cantidad}</td><td>${x.nombre}</td><td>${x.precio}</td></tr>`).join(' ') + `</tbody>
+    </table>`;
+}
+
+document.querySelectorAll(".btn-link")[0].addEventListener('click',e=>{
+    document.location.href = "Usuario - editar cuenta.html"  
+})
+
+document.querySelectorAll(".btn-link")[1].addEventListener('click',e=>{
+    document.location.href = "Usuario - Editar Mascotas.html"  
+})
